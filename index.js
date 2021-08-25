a1ca182e0ce7b533089420a2d0f7adf8dbdceae3
Harley_19957195 <19957195@students.latrobe.edu.au>
2021-08-25 16:45:24 +1000

    Node app with test case

---
+++ index.js
@@ -0,0 +1,13 @@
+// import express framework
+const express = require("express");
+
+const app = express();
+app.get("/", function (req, res) {
+    return res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Server is running on port 8000");
+});
+
+module.exports = app;
